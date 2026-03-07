1cd012cee79569cb26bcf9d86405289315006bdf
Sergio Carvalho <sergio.abreu01@aluno.unifametro.edu.br>
2026-03-07 12:22:45 -0300

    Inclusão dos arquivos do projeto.

---
+++ src/main/java/com/clinica/sertao_api/SertaoApiApplication.java
@@ -0,0 +1,13 @@
+package com.clinica.sertao_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SertaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SertaoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinica/sertao_api/SertaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinica.sertao_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SertaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
