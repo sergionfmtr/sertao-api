e83ad8d3ac4d6172f92ecb56d5c98a51819faadd
Sergio Carvalho <sergio.abreu01@aluno.unifametro.edu.br>
2026-03-14 12:19:47 -0300

    Inclusão da api de paciente.

---
+++ src/main/java/com/clinica/sertao_api/pacientes/Paciente.java
@@ -0,0 +1,50 @@
+package com.clinica.sertao_api.pacientes;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "paciente")
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_paciente")
+    private Long id;
+
+    @NotBlank
+    @Size(max = 150)
+    @Column(nullable = false, length = 150)
+    private String nome;
+
+    @NotBlank
+    @Size(max = 14)
+    @Column(nullable = false, unique = true, length = 14)
+    private String cpf;
+
+    @NotNull
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Size(max = 18)
+    @Column(length = 18)
+    private String telefone;
+
+    @Email
+    @Size(max = 100)
+    @Column(length = 100)
+    private String email;
+} 
+    
